e43eca10070be58b0d3a8bb38400171751eca0de
jkeifer <jarrettkeifer@gmail.com>
2014-05-29 11:23:04 -0700

    moved get_px_coords_from_shapefile to utils; now using function to calc doy from band number; other minor changes.

--- pyhytemporal/classification.py
+++ pyhytemporal/classification.py
@@ -6,22 +6,11 @@ from math import floor
 from datetime import datetime as dt
 from osgeo import gdal
 from osgeo.gdalconst import *
-from osgeo import ogr
 from scipy import interpolate
 from scipy import optimize
 from pyhytemporal.utils import *
 
 gdal.UseExceptions()
-
-
-########## EXCEPTION CLASSES ##########
-
-
-class ShapeDataError(Exception):
-    """
-    Error for wrong geometry type when loading shapefiles
-    """
-    pass
 
 
 ########## OBJECT CLASSES #############
@@ -564,8 +553,6 @@ def process_pixel(bands, bestguess, col, cropname, doyinterval, fitmthd, img, in
     #TODO docstrings
 
     valsf = {}
-    i2 = 0
-    st = startDOY
     hasdata = True
 
     for i in range(0, bands):
@@ -575,15 +562,8 @@ def process_pixel(bands, bestguess, col, cropname, doyinterval, fitmthd, img, in
         if measured == ndvalue:
             hasdata = False
 
-        doy = st + i2 * doyinterval
-
-        if doy > 365:
-            doy = 366
-            i2 = 0
-            st = 366
-
+        doy = band_number_to_doy(i+1, startDOY, doyinterval)
         valsf[doy] = measured
-        i2 += 1
 
     if hasdata:
         bnds = ((0.6, 1.4), (0.6, 1.4), (-10, 10))
@@ -746,158 +726,6 @@ def phenological_classificaion(imagetoprocess, outputdirectory, createfoldername
 
 
 #############################################
-#      GET PX COORDS FROM A SHAPEFILE       #
-#############################################
-
-
-def load_points(shapefile):
-    """
-    Returns a list of coordinate from points in an input shapefile.
-
-    Required Argument(s):
-        - shapefile: The path to a point-geometry shapefile
-
-    Optional Argument(s):
-        - None
-
-    Returns:
-        - points: A list of tuples with the x, y coords for each point in the input shapefile
-    """
-    #TODO Clean up commenting
-
-    # Open shapeData
-    shapeData = ogr.Open(validateShapePath(shapefile))
-    # Validate shapeData
-    validateShapeData(shapeData)
-    # Get the first layer
-    layer = shapeData.GetLayer()
-    # Initialize
-    points = []
-    # For each point,
-    for index in xrange(layer.GetFeatureCount()):
-        # Get
-        feature = layer.GetFeature(index)
-        geometry = feature.GetGeometryRef()
-        # Make sure that it is a point
-        if geometry.GetGeometryType() != ogr.wkbPoint:
-            raise ShapeDataError('This function only accepts point geometry.')
-            # Get pointCoordinates
-        pointCoordinates = geometry.GetX(), geometry.GetY()
-        # Append
-        points.append(pointCoordinates)
-        # Cleanup
-        feature.Destroy()
-        # Cleanup
-    shapeData.Destroy()
-    # Return
-    return points
-
-
-def get_ref_from_shapefile(shapefile):
-    """
-    Gets a spatial reference from an input shapefile.
-
-    Required Arguement(s):
-        - shapefile: The path to a shapefile
-
-    Optional Argument(s):
-        - None
-
-    Returns:
-        - spatialref: The spatial reference of then input shapefile in proj4 format
-    """
-
-    # Open shapeData
-    shapeData = ogr.Open(validateShapePath(shapefile))
-    # Validate shapeData
-    validateShapeData(shapeData)
-    # Get the first layer
-    layer = shapeData.GetLayer()
-    # Get spatial reference as proj4
-    spatialref = layer.GetSpatialRef()
-
-    return spatialref
-
-
-def getSpatialReferenceFromProj4(proj4):
-    """Return GDAL spatial reference object from proj4 string"""
-    #TODO: docstrings
-
-    spatialReference = ogr.SpatialReference()
-    spatialReference.ImportFromProj4(proj4)
-    return spatialReference
-
-
-# Validate
-
-def validateShapePath(shapePath):
-    """Validate shapefile extension"""
-    #TODO: docstrings
-
-    return os.path.splitext(str(shapePath))[0] + '.shp'
-
-
-def validateShapeData(shapeData):
-    """Make sure we can access the shapefile"""
-
-    #TODO: docstrings
-
-    # Make sure the shapefile exists
-    if not shapeData:
-        raise ShapeDataError('The shapefile is invalid')
-        # Make sure there is exactly one layer
-    if shapeData.GetLayerCount() != 1:
-        raise ShapeDataError('The shapefile must have exactly one layer')
-
-
-def check_spatial_refs(srs1, srs2):
-    if srs1 == srs2:
-        return False
-    else:
-        return True
-
-
-def get_px_coords_from_points(raster, shapefile):
-    """
-    Takes geographic coordinates from a shapefile and finds the corresponding pixel coordinates on a raster.
-
-    rst = "/Users/phoetrymaster/Documents/School/Geography/Thesis/Data/MODIS_KANSAS_2007-2012/reprojected/clips/KansasEVI_2012_clip1.tif"
-    #rst = "/Users/phoetrymaster/Documents/School/Geography/Thesis/Data/polygonclip_20130929223024_325071991/resampled/newclips/2012clip1.tif"
-    shp = "/Users/phoetrymaster/Documents/School/Geography/Thesis/Data/MODIS_KANSAS_2007-2012/SampleAreas/samplepoints2012_clip1_new.shp"
-
-    print get_px_coords_from_points(rst, shp)
-    """
-    #TODO docstrings
-
-    pointcoords = load_points(shapefile)
-    ref1 = get_ref_from_shapefile(shapefile)
-
-    image = gdalObject
-    image.open(raster)
-    image.close()
-
-    referror = check_spatial_refs(ref1, image.projection)
-
-    if referror:
-        print "WARNING: Spatial Reference of raster does not match points shapefile. Output may not be as expected. For best resutls ensure reference systems are identical."
-        #TODO Change to use warnings module
-
-    #Raster edge coords
-    left = image.geotransform[0]
-    top = image.geotransform[3]
-    right = image.cols * image.geotransform[1] + image.geotransform[0]
-    bottom = image.cols * image.geotransform[5] + image.geotransform[3]
-
-    pxcoords = []
-    for coords in pointcoords:
-        x = int(floor(image.cols * (coords[0] - left) / (right - left)))
-        y = int(floor(image.rows * (coords[1] - top) / (bottom - top)))
-        pxcoords.append((x, y))
-
-    return pxcoords
-
-
-#############################################
 #       Classify and Assess Accuracy        #
 #############################################
 
@@ -1039,7 +867,7 @@ def classify_with_threshold(croparray, filelist, searchdir, searchstringsvals, t
     return accuracy, classification, cols, rows, outstring
 
 
-def main(searchdir, cropimgpath, searchstringsvals, nodata, outdir=None, outfilename=None):
+def classify_and_assess_accuracy(searchdir, cropimgpath, searchstringsvals, nodata, outdir=None, outfilename=None):
     if outdir is None:
         outdir = searchdir
     else:
@@ -1058,7 +886,7 @@ def main(searchdir, cropimgpath, searchstringsvals, nodata, outdir=None, outfile
         #np.set_printoptions(threshold=np.nan)
 
         #Crop image is constant for all iterations
-        cropimg = gdalObject
+        cropimg = gdalObject()
         cropimg.open(cropimgpath)
         band = cropimg.gdal.GetRasterBand(1)
         croparray = band.ReadAsArray(0, 0, cropimg.cols, cropimg.rows)
@@ -1134,6 +962,9 @@ def main(searchdir, cropimgpath, searchstringsvals, nodata, outdir=None, outfile
 
 
 def get_crop_pixel_values(imagepath, locations):
+
+    #TODO docstring
+
     gdal.AllRegister()
     img = gdal.Open(imagepath, GA_ReadOnly)
 
@@ -1173,19 +1004,17 @@ def write_refs_to_txt(cropname, referencevalues, startdoy, doyinterval, outdir, 
         for points in referencevalues:
             f.write("\nPoint {0}:\n".format(point))
             point += 1
-            imgnumber = 0
-            st = startdoy
+            imgnumber = 1
             for val in points:
-                doy = st + imgnumber * doyinterval
-                if doy > 365:
-                    doy = 366
-                    st = 366
-                    imgnumber = 0
+                doy = band_number_to_doy(imgnumber, startdoy, doyinterval)
                 f.write("{0} {1}\n".format(doy, val))
                 imgnumber += 1
 
 
 def write_mean_ref_to_txt(cropname, referencevalues, startdoy, doyinterval, outdir, comment="", postfix=""):
+
+    #TODO docstring
+
     print "Writing mean reference curve to output file:"
     print referencevalues
 
@@ -1194,37 +1023,40 @@ def write_mean_ref_to_txt(cropname, referencevalues, startdoy, doyinterval, outd
         if comment:
             f.write("//" + comment + "\n\n")
         meanvals = get_mean_values(referencevalues)
-        imgnumber = 0
-        st = startdoy
+        imgnumber = 1
         for val in meanvals:
-            doy = int(startdoy) + imgnumber * int(doyinterval)
-            if doy > 365:
-                doy = 366
-                st = 366
-                imgnumber = 0
+            doy = band_number_to_doy(imgnumber, startdoy, doyinterval)
             f.write("{0} {1}\n".format(doy, val))
             imgnumber += 1
 
 
 def get_mean_values(referencevalues):
+
+    #TODO docstring
+
     array = numpy.array(referencevalues)
     mean = numpy.mean(array, axis=0)
     return list(mean)
 
 
 def get_reference_curves(image, refstoget, startdoy, imageinterval, outdir="", filepostfix=""):
+    """
+
+    """
+
+    #TODO docstring
+
     if not outdir:
         outdir = os.path.dirname(image)
 
-    for key, val in refstoget.items():
-        if val:
-            cropname = key
-            locs = val
-            refvals = get_crop_pixel_values(image, locs)
-            comment = "Generated from {0} by get_crop_pixel_values version 0.1.".format(image)
-            write_refs_to_txt(cropname, refvals, startdoy, imageinterval, outdir, comment=comment, postfix=filepostfix)
-            write_mean_ref_to_txt(cropname, refvals, startdoy, imageinterval, outdir, comment=comment,
-                                  postfix=filepostfix)
+    for shapefile in refstoget:
+        cropname = os.path.splitext(os.path.basename(shapefile))[0]
+        locs = get_px_coords_from_points(image, shapefile)
+        refvals = get_crop_pixel_values(image, locs)
+        comment = "Generated from {0} by get_crop_pixel_values version 0.1.".format(image)
+        write_refs_to_txt(cropname, refvals, startdoy, imageinterval, outdir, comment=comment, postfix=filepostfix)
+        write_mean_ref_to_txt(cropname, refvals, startdoy, imageinterval, outdir, comment=comment,
+                              postfix=filepostfix)
 
 
 ########## PROCEDURE ##########
