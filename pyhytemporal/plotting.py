c2b5483c588fcfeb07f73653910432153e156188
jkeifer <jarrettkeifer@gmail.com>
2014-07-12 15:49:02 -0700

    exceptions classes, font styling, legends, and bugfixes

--- pyhytemporal/plotting.py
+++ pyhytemporal/plotting.py
@@ -1,11 +1,35 @@
 import os
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from matplotlib.backends.backend_pdf import PdfPages
 #from core import signatureCollection, temporalSignature
 
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 4}
+
+rc('font', **font)
+
+
+class FigureError(Exception):
+    """
+    Error for figure opening/closing in plotting lib
+    """
+    pass
+
+
+class LegendError(Exception):
+    """
+    Error for figure opening/closing in plotting lib
+    """
+    pass
+
 
 def plot_color_picker(i):
+    from matplotlib.colors import cnames
+    from random import shuffle
     colorcodes = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    colorcodes.extend(cnames.keys())
     i = i % len(colorcodes)
 
     return colorcodes[i]
@@ -16,12 +40,21 @@ class Plot(object):
         #TODO: Validate Directory
         self.pdf = PdfPages(os.path.join(outputpath, name + ".pdf"))
         self.figure = None
+        self.lines = []
+
+    def close_plot(self):
+        try:
+            self.close_figure()
+        except FigureError as e:
+            print(e)
+
+        self.pdf.close()
 
     def create_figure(self):
         if not self.figure:
             self.figure = plt.figure()
         else:
-            raise Exception("Error: figure already exists. Close the figure to create a new figure.")
+            raise FigureError("Error: figure already exists. Close the figure to create a new figure.")
 
     def close_figure(self, save=True):
         if self.figure:
@@ -29,15 +62,13 @@ class Plot(object):
                 self.pdf.savefig()
             plt.close(self.figure)
         else:
-            raise Exception("Error: no figure is open. Cannot close nothing.")
+            raise FigureError("Error: no figure is open. Cannot close nothing.")
 
-    def close_plot(self):
-        try:
-            self.close_figure()
-        except:
-            pass
-
-        self.pdf.close()
+    def add_legend(self):
+        if self.lines:
+            plt.legend()#labels=self.labels)
+        else:
+            raise LegendError("Error: No labels to add to the legend.")
 
 
 class PixelPlot(Plot):
@@ -52,10 +83,10 @@ class PixelPlot(Plot):
 
         try:
             self.create_figure()
-        except:
+        except FigureError:
             pass
 
-        axes = self.figure.add_subplot(1, 1, 1)
+        axes = self.figure.add_subplot(0.5, 0.5, 0.5)
         axes.plot(pixelObject.values, pixelObject.bandDOYs, color)
         axes.set_xlabel("Pixel row {0} col {1}: {2} as {3} in classified".format(pixelObject.row,
                                                                                  pixelObject.col,
@@ -68,24 +99,24 @@ class PixelPlot(Plot):
 class SignaturePlot(Plot):
     def plot_signature(self, temporalSignature, color=None, closefigure=False):
 
-        if color:
-            color = '{0}-'.format(color)
-        else:
-            color = "black-"
+        if not color:
+            color = "k"
 
         try:
             self.create_figure()
-        except:
+        except FigureError:
             pass
 
-        axes = self.figure.add_subplot(1,1,1)
-        axes.plot(temporalSignature.daysofyear, temporalSignature.vivalues, color)
-        axes.set_xlabel("Temporal signature {0}".format(temporalSignature.name))
+        print color
+        axes = self.figure.add_subplot(1, 1, 1)
+        self.lines.append(axes.plot(temporalSignature.daysofyear, temporalSignature.vivalues,
+                                    color=color, linestyle='-', label=temporalSignature.name))
+        #axes.set_xlabel("Temporal signature {0}".format(temporalSignature.name))
 
         if closefigure is True:
             self.close_figure()
 
-    def plot_collection(self, signatureCollection, insamefigure=True):
+    def plot_collection(self, signatureCollection, insamefigure=True, legend=True, closeplot=True):
 
         if insamefigure:
             closefig = False
@@ -93,4 +124,10 @@ class SignaturePlot(Plot):
         for i, signature in enumerate(signatureCollection.signatures):
             color = plot_color_picker(i)
             self.plot_signature(signature, color=color, closefigure=closefig)
+
+        if legend:
+            self.add_legend()
+
+        if closeplot:
+            self.close_plot()
 
